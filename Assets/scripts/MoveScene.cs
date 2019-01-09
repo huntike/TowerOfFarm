dca6fbd0126fcdc0a155dd1c39d553faa1ac024f
huntike <logan.lelay@orange.fr>
2019-01-09 10:35:00 +0100

    modification map dj et ajout scenemanagement

---
+++ Assets/scripts/MoveScene.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MoveScene : MonoBehaviour {
+    [SerializeField] private string SceneLevel;
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            SceneManager.LoadScene(SceneLevel);
+        }
+    }
+}
