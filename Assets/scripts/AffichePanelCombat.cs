21e6995a1eaceedf94e5fd1815cc3648934aab37
Jérémy SUAU <jeremysuau>
2019-03-20 16:38:08 +0100

    ajout interface

---
+++ Assets/scripts/AffichePanelCombat.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AffichePanelCombat : MonoBehaviour {
+	
+	public GameObject armepanel;
+	public GameObject sortpanel;
+	public GameObject potionpanel;
+	public GameObject fuirpanel;
+	public GameObject infopanel;
+	
+	// Use this for initialization
+	void Start () {
+		armepanel.SetActive(false);
+
+		sortpanel.SetActive(false);
+
+		potionpanel.SetActive(false);
+
+		fuirpanel.SetActive(false);
+
+		infopanel.SetActive(false);
+	}
+}
---
+++ Assets/scripts/SwitchPanel.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchPanel : MonoBehaviour {
+
+	public void AfficheHero(GameObject HeroPanel)
+	{
+		HeroPanel.SetActive(!HeroPanel.activeSelf);
+	}
+	public void EnleveHero(GameObject HeroPanel)
+	{
+		HeroPanel.SetActive(!HeroPanel.activeSelf);
+	}
+	//------------------------------------------------
+	public void AfficheArmes(GameObject ArmesPanel)
+	{
+		ArmesPanel.SetActive(!ArmesPanel.activeSelf);
+	}
+	public void EnleveArmes(GameObject ArmesPanel)
+	{
+		ArmesPanel.SetActive(!ArmesPanel.activeSelf);
+	}
+	//-------------------------------------------------
+	public void AfficheSort(GameObject SortPanel)
+	{
+		SortPanel.SetActive(!SortPanel.activeSelf);
+	}
+	public void EnleveSort(GameObject SortPanel)
+	{
+		SortPanel.SetActive(!SortPanel.activeSelf);
+	}
+	//------------------------------------------------
+	public void AffichePotion(GameObject PotionPanel)
+	{
+		PotionPanel.SetActive(!PotionPanel.activeSelf);
+	}
+	public void EnlevePotion(GameObject PotionPanel)
+	{
+		PotionPanel.SetActive(!PotionPanel.activeSelf);
+	}
+	//------------------------------------------------
+	public void AfficheFuir(GameObject FuirPanel)
+	{
+		FuirPanel.SetActive(!FuirPanel.activeSelf);
+	}
+	public void EnleveFuir(GameObject FuirPanel)
+	{
+		FuirPanel.SetActive(!FuirPanel.activeSelf);
+	}
+	//------------------------------------------------
+	public void AfficheInfo(GameObject InfoPanel)
+	{
+		InfoPanel.SetActive(!InfoPanel.activeSelf);
+	}
+	public void EnleveInfo(GameObject InfoPanel)
+	{
+		InfoPanel.SetActive(!InfoPanel.activeSelf);
+	}
+	//------------------------------------------------
+	public void AfficheMenu(GameObject MenuPanel)
+	{
+		MenuPanel.SetActive(!MenuPanel.activeSelf);
+	}
+	public void EnleveMenu(GameObject MenuPanel)
+	{
+		MenuPanel.SetActive(!MenuPanel.activeSelf);
+	}
+	//------------------------------------------------
+	public void AfficheParametre(GameObject ParametrePanel)
+	{
+		ParametrePanel.SetActive(!ParametrePanel.activeSelf);
+	}
+	public void EnleveParametre(GameObject ParametrePanel)
+	{
+		ParametrePanel.SetActive(!ParametrePanel.activeSelf);
+	}
+	//------------------------------------------------
+	public void AfficheComfirm(GameObject ComfirmPanel)
+	{
+		ComfirmPanel.SetActive(!ComfirmPanel.activeSelf);
+	}
+	public void EnleveComfirm(GameObject ComfirmPanel)
+	{
+		ComfirmPanel.SetActive(!ComfirmPanel.activeSelf);
+	}
+	//------------------------------------------------
+	public void AfficheComfirmQuit(GameObject ComfirmQuitPanel)
+	{
+		ComfirmQuitPanel.SetActive(!ComfirmQuitPanel.activeSelf);
+	}
+	public void EnleveComfirmQuit(GameObject ComfirmQuitPanel)
+	{
+		ComfirmQuitPanel.SetActive(!ComfirmQuitPanel.activeSelf);
+	}
+}
